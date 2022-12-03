45ce768bf9bed559d6f2d219b873af4276a5e1aa
lauromiottojr <junior.miotto36@gmail.com>
2022-12-03 13:22:28 -0300

    Configurando projeto e adicionando imagens

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Footer from './components/layout/Footer';
-import Navbar from './components/layout/Navbar';
-import Contato from './pages/Contato';
-import Empresa from './pages/Empresa';
-import Home from './pages/Home';
-
-
 function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/empresa' element={<Empresa />} />
-          <Route path='/contato' element={<Contato />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <h1>tudo certo</h1>
     </div>
   );
 }
