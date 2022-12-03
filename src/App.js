1f8212bfb77a2b380170d0ce97904d1579b35d66
lauromiottojr <junior.miotto36@gmail.com>
2022-12-03 11:29:17 -0300

    Adicionando icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
 import Contato from './pages/Contato';
 import Empresa from './pages/Empresa';
 import Home from './pages/Home';
@@ -10,16 +11,13 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/empresa">Empresa</Link></li>
-          <li><Link to="/contato">Contato</Link></li>
-        </ul>
+        <Navbar />
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/empresa' element={<Empresa />} />
           <Route path='/contato' element={<Contato />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
