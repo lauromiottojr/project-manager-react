16bf9daadbbeddd10d8f6df08a2c1c7f9ece3486
lauromiottojr <junior.miotto36@gmail.com>
2022-12-05 11:20:58 -0300

    Finalizando componente Home e criando componente LinkButton

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,13 @@
 import styles from './Home.module.css'
 import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
 
 function Home() {
     return (
         <section className={styles.homeContainer}>
             <h1>Bem-vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-            <a href="/">Criar projeto</a>
+            <LinkButton to="/newproject" text="Criar projecto" />
             <img src={savings} alt="Costs" />
         </section>
     )
