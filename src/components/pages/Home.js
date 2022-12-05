9407629e13c5cad48b1c9a8172e8e20ff5359203
lauromiottojr <junior.miotto36@gmail.com>
2022-12-05 11:04:12 -0300

    Criando e estilizando componente Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,14 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+
 function Home() {
     return (
-        <>
-            <h2>Home</h2>
-        </>
+        <section className={styles.homeContainer}>
+            <h1>Bem-vindo ao <span>Costs</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <a href="/">Criar projeto</a>
+            <img src={savings} alt="Costs" />
+        </section>
     )
 }
 export default Home
