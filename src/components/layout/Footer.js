4d3ead81baf464f3d67472d8f032292eceedc6be
lauromiottojr <junior.miotto36@gmail.com>
2022-12-05 10:30:51 -0300

    Criando e estilizando componente Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,10 +1,22 @@
 import styles from './Footer.module.css'
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
 
 function Footer() {
     return (
-        <>
-            <p>foi</p>
-        </>
+        <footer className={styles.footer}>
+            <ul className={styles.socialList}>
+                <li>
+                    <FaFacebook />
+                </li>
+                <li>
+                    <FaInstagram />
+                </li>
+                <li>
+                    <FaLinkedin />
+                </li>
+            </ul>
+            <p className={styles.copyRight}><span>Costs</span> &copy; 2022</p>
+        </footer>
     )
 }
 
