f3e7629b40aa8a07a5b2b2240d1f26ac8316cd49
lauromiottojr <junior.miotto36@gmail.com>
2022-12-05 21:25:50 -0300

    Criando componentes Select e SubmitButton

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -7,7 +7,7 @@ function NewProject() {
         <div className={styles.newprojectContainer}>
             <h1>Criar projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm />
+            <ProjectForm btnText="Criar projeto" />
         </div>
     )
 }
--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -1,20 +1,16 @@
 import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
-function ProjectForm() {
+function ProjectForm({ btnText }) {
     return (
         <form className={styles.form}>
             <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" />
             <Input type="number" text="Orçamento do projeto" name="budget"
                 placeholder="Insira o orçamento total" />
-            <div>
-                <select name="categoryId">
-                    <option disabled>Selecione a categoria</option>
-                </select>
-            </div>
-            <div>
-                <input type="submit" value="Criar projeto" />
-            </div>
+            <Select name="categoryId" text="Selecione a categoria" />
+            <SubmitButton text={btnText} />
         </form>
     )
 }
