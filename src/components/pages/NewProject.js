a381b607398c208b91bee43be4993e9e65882e89
lauromiottojr <junior.miotto36@gmail.com>
2022-12-06 09:55:26 -0300

    Metodo POST para novo projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,8 +1,32 @@
+import { useHistory } from 'react-router-dom'
+
 import ProjectForm from '../projects/ProjectForm'
 
 import styles from './NewProject.module.css'
 
 function NewProject() {
+
+    const history = useHistory()
+
+    function createPost(project) {
+        // initialize costs and services
+        project.cost = 0
+        project.service = []
+
+        fetch("http://localhost:5000/projects", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        }).then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                //redirect
+            })
+            .catch((err) => console.log(err))
+    }
+
     return (
         <div className={styles.newprojectContainer}>
             <h1>Criar projeto</h1>
