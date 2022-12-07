b02970af878cade356ab30045a86e5d131f6ebee
lauromiottojr <junior.miotto36@gmail.com>
2022-12-07 10:47:16 -0300

    Configurando mensagem ao editar Projeto

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import Loading from '../layout/Loading'
 import Container from '../layout/Container'
 import ProjectForm from '../projects/ProjectForm'
+import Message from '../layout/Message'
 
 import styles from './Project.module.css'
 
@@ -12,6 +13,8 @@ function Project() {
     const { id } = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [message, setMessage] = useState()
+    const [type, setType] = useState()
 
     useEffect(() => {
         fetch(`http://localhost:5000/projects/${id}`, {
@@ -32,10 +35,12 @@ function Project() {
     function editPost(project) {
         //budget validation
         if (project.budget < project.cost) {
-            //message
+            setMessage('O orçamento não pode ser menor que o custo do projeto!')
+            setType('error')
+            return false
         }
         fetch(`http://localhost:5000/projects/${project.id}`, {
-            method: "PATCH", // método patch só atualiza o que você alterar
+            method: "PATCH", // verbo HTTP patch só atualiza o que você alterar
             headers: {
                 'Content-Type': 'application/json'
             },
@@ -44,7 +49,8 @@ function Project() {
             .then((data) => {
                 setProject(data)
                 setShowProjectForm(false)
-                // message
+                setMessage('Projeto atualizado!')
+                setType('success')
             })
             .catch(err => console.log(err))
     }
@@ -54,6 +60,7 @@ function Project() {
             {project.name ? (
                 <div className={styles.projectDetails}>
                     <Container customClass="column">
+                        {message && <Message type={type} msg={message} />}
                         <div className={styles.detailsContainer}>
                             <h1>Projeto: {project.name}</h1>
                             <button className={styles.btn} onClick={toggleProjectForm}>
