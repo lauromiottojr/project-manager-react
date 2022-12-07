734831740f7f4c19e6006abf0c602856ff5e9566
lauromiottojr <junior.miotto36@gmail.com>
2022-12-06 23:02:33 -0300

    Estilizando Project Edit

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,12 +1,16 @@
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
+
 import styles from './Project.module.css'
 
 function Project() {
 
     const { id } = useParams()
     const [project, setProject] = useState([])
+    const [showProjectForm, setShowProjectForm] = useState(false)
 
     useEffect(() => {
         fetch(`http://localhost:5000/projects/${id}`, {
@@ -21,8 +25,36 @@ function Project() {
             .catch(err => console.log(err))
     }, [id])
 
+    function toggleProjectForm() {
+        setShowProjectForm(!showProjectForm)
+    }
+
     return (
-        <p>{project.name}</p>
+        <>
+            {project.name ? (
+                <div className={styles.projectDetails}>
+                    <Container customClass="column">
+                        <div className={styles.detailsContainer}>
+                            <h1>Projeto: {project.name}</h1>
+                            <button className={styles.btn} onClick={toggleProjectForm}>
+                                {!showProjectForm ? 'Editar projeto' : 'Fechar'}
+                            </button>
+                            {!showProjectForm ? (
+                                <div className={styles.projectInfo}>
+                                    <p><span>Categoria: </span>{project.category.name}</p>
+                                    <p><span>Total de orçamento: </span>R${project.budget}</p>
+                                    <p><span>Total de orçamento utilizado: </span>R${project.cost}</p>
+                                </div>
+                            ) : (
+                                <div className={styles.projectInfo}>detalhes projeto</div>
+                            )}
+                        </div>
+                    </Container>
+                </div>
+            ) : (
+                <Loading />
+            )}
+        </>
     )
 }
 
